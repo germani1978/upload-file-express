fc5d2d5acb7a1fb38f051179be4c83242fb09884
germani1978 <germanibenitezalfonso1@gmail.com>
2022-12-19 11:17:07 -0500

    second

---
+++ index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var cors = require('cors');
+const { send } = require('express/lib/response');
+require('dotenv').config()
+const multer = require('multer');
+
+
+var app = express();
+
+const upload = multer({ dest: './public/date/uploads'});
+
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+app.post('/api/fileanalyse',upload.single('upfile'), (req, res) => {
+
+  const { originalname, mimetype, size } = req.file;
+
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size:size
+  });
+});
+ 
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port);
+});
